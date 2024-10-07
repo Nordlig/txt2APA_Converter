34345d634155154e378b8b3a018a44a0c36f804e
Ivar Bergelin <ivar.bergelin@gmail.com>
2024-10-07 13:17:42 +0200

    revisió final

--- biblio.py
+++ biblio.py
@@ -1,7 +1,10 @@
 import os
 import re
-import unicodedata
-from tqdm import tqdm
+#import unicodedata
+#from tqdm import tqdm
+import codecs
+
+prefijos = ["de la", "de las", "de los", "del", "di", "della", "da", "de", "du", "des","e","van der","van de", "van den"]
 
 def detectar_etiqueta_html(palabra):
     """
@@ -141,51 +144,105 @@ def procesar_cadena(cadena):
     resultado_cadena = ' '.join(resultado)
     return resultado_cadena
 
+def format_iniciales(iniciales):
+    # Formatear iniciales con puntos
+    return ' '.join(f"{i}." for i in iniciales)
+
+    
+def capitalizar_primera_y_tercera_palabra(apellido):
+    partes = apellido.split()    
+    if not '-' in partes[2]:
+        partes[0] = partes[0].capitalize()
+        partes[1] = partes[1].lower()       
+        partes[2] = partes[2].capitalize()  # Capitaliza la tercera palabra
+
+    return ' '.join(partes)
+
+def format_iniciales(iniciales):
+    # Asumiendo que quieres formatear las iniciales con puntos
+    return '.'.join(iniciales) + '.'
+
 def procesar_autores(autores):
-    autores_formateados = []
-
+        
+    excepciones_mac_minusculas = [
+        "Macmillan", "Macgregor", "Macfarlane", "Macpherson", "Mackenzie", "Macleod", "Macintyre", "Macdonald",
+        "Macintosh", "Macdougall", "Maccallum", "Macalister", "Macadam", "Macarthur", "Macbeth", "Macbeth",
+        "Macquarie", "Maclachlan", "Macgillivray", "Maclean", "Maccabees", "Maccarthy", "Maccoy",
+        "Macneil", "Macnichol", "Macnicol", "Macrory", "Maclaren", "Macduff", "Macewan", "Maceachern"
+    ]
+    
+    resultados = []
+    partes = autores.split()
+    apellido_actual = []
     i = 0
-    while i < len(autores):
-        # Verificar si el autor es [Anonymous]
-        if autores[i] == "[Anonymous]":
-            autores_formateados.append("[Anonymous]")
+    buffer = ""
+    
+    while i < len(partes):
+        # Verificar si es "[Anonymous]"
+        if partes[i] == "[Anonymous]":
+            resultados.append(f"[Anonymous]")
             break
-
-        # Manejar apellido compuesto
-
-        # Capitalizar todos los apellidos compuestos y sepaados con "-" 
-        apellido_parts = [part.strip().capitalize() for part in autores[i].split('-')]
-        apellido = '-'.join(apellido_parts)
-
-        # Verificar si hay al menos un elemento más después del apellido del autor actual
-        if i + 1 < len(autores):
-            siguiente = autores[i + 1]
-            if ',' in siguiente:
-                apellido += f" {siguiente}"
-                i += 1  # Saltar siguiente elemento en caso de apellido compuesto
-
-            # Obtener las iniciales si hay al menos un elemento más después del apellido del autor actual
-            if i + 1 < len(autores):
-                iniciales = autores[i + 1]
-
+        
+        # Acumulación en el buffer
+        if buffer:
+            buffer += " "
+        buffer += partes[i]
+        
+        # Verificar si la parte actual termina en coma
+        if partes[i].endswith(','):
+            apellido = buffer[:-1]  # Elimina la coma final
+            # Verificar si el apellido contiene algún prefijo
+            if any(apellido.lower().startswith(prefijo + " ") for prefijo in prefijos):
+                tros = apellido.split()
+                if len(tros) > 2:
+                    apellido = capitalizar_primera_y_tercera_palabra(apellido)
+                    apellido_actual.append(apellido)
+                else:
+                    apellido_actual.append(apellido.title())
+                    
+            elif apellido.upper().startswith("MAC") and apellido.capitalize() in excepciones_mac_minusculas:
+                apellido_actual.append(apellido.capitalize())               
+            elif apellido.upper().startswith("MAC"):
+                apellido_actual.append("Mac" + apellido[3:].capitalize())               
+            elif apellido.upper().startswith("MC"):
+                apellido_actual.append("Mc" + apellido[2:].capitalize())               
+            
+            elif apellido.startswith("O'"):
+                apellido_actual.append(apellido)
+            
+            elif "-" in apellido:
+                apellido_actual.append(apellido)
+                
             else:
-                iniciales = None
-        else:
-            iniciales = None
+                apellido_actual.append(apellido.title())
 
-        # Verificar si hay iniciales
-        if iniciales:
-            iniciales_formateadas = '. '.join(iniciales)
-            autor_formateado = f"{apellido} {iniciales_formateadas}."
-        else:
-            autor_formateado = f"{apellido}."
-
-        autores_formateados.append(autor_formateado)
-
-        i += 2  # Avanzar dos posiciones para pasar al siguiente autor
-
-    return autores_formateados
-
+            # Limpiar el buffer para el siguiente apellido
+            buffer = ""
+            i += 1
+                          
+            # Manejo de iniciales
+            if i < len(partes) and not partes[i].endswith(','):
+                iniciales = partes[i]
+                iniciales_formateadas = format_iniciales(iniciales)
+                nombre_completo = ' '.join(apellido_actual)
+                resultados.append(f"{nombre_completo}, {iniciales_formateadas}")
+                apellido_actual = []
+                i += 1
+            else:               
+                nombre_completo = ' '.join(apellido_actual)
+                resultados.append(f"{nombre_completo} ")
+                apellido_actual = []
+        else: 
+            #apellido_actual.append(buffer)
+            i += 1
+    
+    # Manejar el caso final si queda algo en el buffer
+    if apellido_actual:
+        nombre_completo = ' '.join(apellido_actual)
+        resultados.append(f"{nombre_completo} ")
+        
+    return resultados
+  
 def informacion_procesada(ruta_del_archivo):
         
     tags_principales = [
@@ -201,78 +258,86 @@ def informacion_procesada(ruta_del_archivo):
 
     # Claves obligatorias
     claves_obligatorias = ["AU", "PY", "TI", "SO", "VL", "BP", "EP"]
-    
-    # Abre el archivo en modo lectura
-    with open(ruta_del_archivo, "r") as archivo:
-        
-        # Inicializa una lista para almacenar los diccionarios
-        lista_diccionarios = []
-        # Inicializa el diccionario actual para almacenar los datos entre PT y ER
-        diccionario_actual = {}
-        # Itera sobre cada línea del archivo
-        for linea in archivo:
-            # Elimina espacios en blanco al principio y al final de la línea
-            linea = linea.strip()
-            palabras = linea.split(maxsplit=1)
 
-            # Verifica si la línea comienza con una etiqueta principal
-            if palabras and palabras[0] in tags_principales:
-                # Si es una etiqueta principal, establece la clave_actual
-                clave_actual = palabras[0]
-                # Si hay datos después de la etiqueta, guárdalos como el valor para esa clave
-                if len(palabras) > 1:
-                    valor_actual = ' '.join(palabras[1:]).strip()
-                    # Inicializa la clave en el diccionario actual si no existe
-                    diccionario_actual.setdefault(clave_actual, "")
-                    # Agrega los valores a la clave actual en el diccionario actual
-                    diccionario_actual[clave_actual] += " " + valor_actual
-                else:
-                    diccionario_actual[clave_actual] = ""
+    # Intenta abrir el archivo con utf-8-sig para manejar archivos con BOM
+    try:
+        with codecs.open(ruta_del_archivo, 'r', encoding='utf-8-sig') as archivo:
+            contenido = archivo.read()
+            print('Archivo en formato UTF-8-BOM')
+    except UnicodeDecodeError:
+        # Si falla, intenta abrirlo con utf-8 estándar
+        with open(ruta_del_archivo, 'r', encoding='utf-8') as archivo:
+            contenido = archivo.read()
+            print('Archivo en formato UTF-8')
 
-            elif clave_actual is not None:
-                # Si no es una clave, asume que es un valor y lo agrega al valor actual del diccionario actual
-                diccionario_actual.setdefault(clave_actual, "")  # Inicializa la clave si no existe
-                diccionario_actual[clave_actual] += " " + linea
+    # Inicializa una lista para almacenar los diccionarios
+    lista_diccionarios = []
+    # Inicializa el diccionario actual para almacenar los datos entre PT y ER
+    diccionario_actual = {}
+    # Itera sobre cada línea del archivo
+    lineas = contenido.split('\n')
+    clave_actual = None
+
+    for linea in lineas:
+        # Elimina espacios en blanco al principio y al final de la línea
+        linea = linea.strip()
+        palabras = linea.split(maxsplit=1)
+
+        # Verifica si la línea comienza con una etiqueta principal
+        if palabras and palabras[0] in tags_principales:
+            # Si es una etiqueta principal, establece la clave_actual
+            clave_actual = palabras[0]
+            # Si hay datos después de la etiqueta, guárdalos como el valor para esa clave
+            if len(palabras) > 1:
+                valor_actual = ' '.join(palabras[1:]).strip()
+                # Inicializa la clave en el diccionario actual si no existe
+                diccionario_actual.setdefault(clave_actual, "")
+                # Agrega los valores a la clave actual en el diccionario actual
+                diccionario_actual[clave_actual] += " " + valor_actual
+            else:
+                diccionario_actual[clave_actual] = ""
+
+        elif clave_actual is not None:
+            # Si no es una clave, asume que es un valor y lo agrega al valor actual del diccionario actual
+            diccionario_actual.setdefault(clave_actual, "")  # Inicializa la clave si no existe
+            diccionario_actual[clave_actual] += " " + linea
+
+        if clave_actual == 'ER':
+            
+            if all(diccionario_actual.get(clave, "") != "" for clave in claves_obligatorias):
+                # Procesar los autores de acuerdo al formato de entrada
+                if '\t' in diccionario_actual['AU']:
+                    autores = (diccionario_actual['AU'].split('\t'))
+                # Si hay un Autor sin , al final añadimos una
+                if "," not in diccionario_actual['AU'] and "[Anonymous]" not in diccionario_actual['AU']:
+                    diccionario_actual['AU'] = diccionario_actual['AU']+','
+                    autores = diccionario_actual['AU']
                 
-            if clave_actual == 'ER':
-
-                if all(diccionario_actual.get(clave, "") != "" for clave in claves_obligatorias):
+                else:
+                    autores = diccionario_actual['AU']
                     
-                    # Procesar los autores de acuerdo al formato de entrada
-                    if '\t' in diccionario_actual['AU']:
-                        autores = diccionario_actual['AU'].split('\t')
-                    else:
-                        autores = diccionario_actual['AU'].split()
+                autores_formateados = procesar_autores(autores)
+                
+                # Agregar ", &" entre los autores si hay más de uno
+                if len(autores_formateados) > 1:
+                    diccionario_actual['AU'] = ', & '.join(autores_formateados)
+                else:
+                    diccionario_actual['AU'] = autores_formateados[0]
 
-                    autores_formateados = procesar_autores(autores)
-
-                    # Agregar ", &" entre los autores si hay más de uno
-                    if len(autores_formateados) > 1:
-                        diccionario_actual['AU'] = ', & '.join(autores_formateados)
-                    else:
-                        diccionario_actual['AU'] = autores_formateados[0]
-
-
-                    # Agrega el diccionario actual a la lista de diccionarios
-                    if diccionario_actual['AU'] != "[Anonymous]" and "Article" in diccionario_actual['DT']: 
+                # Agrega el diccionario actual a la lista de diccionarios
+                if diccionario_actual['AU'] != "[Anonymous]" and 'DT' in diccionario_actual: 
+                    if "Article" in diccionario_actual['DT']:
                         lista_diccionarios.append(diccionario_actual)
 
+            # Reiniciamos el diccionario actual
+            diccionario_actual = {}
 
-                # Reiniciamos el diccionario actual
-                diccionario_actual = {}
+    return lista_diccionarios
 
-        # Elimina las claves no deseadas de cada diccionario en la lista
-        claves_obligatorias = ["AU", "PY", "TI", "SO", "VL", "IS", "BP", "EP", "DE", "ID"]
-        for diccionario in lista_diccionarios:
-            for key in list(diccionario.keys()):
-                if key not in claves_obligatorias:
-                    del diccionario[key]
-
-        return lista_diccionarios
 
 def exportar_a_archivo(datos, nombre_archivo_salida='newrefs.txt'):
     with open(nombre_archivo_salida, 'w', encoding = 'ansi') as archivo_salida:
-
+    #with open(nombre_archivo_salida, 'w') as archivo_salida:
         # Odenamos alfabbeticamente los autores
         for diccionario in sorted(datos, key=lambda x: x.get('AU', '').strip()):
 
@@ -316,10 +381,10 @@ if __name__ == "__main__":
         informacion_procesada_resultado = informacion_procesada(ruta_del_archivo)
 
         # Inicializar la barra de progreso con el número total de diccionarios
-        with tqdm(total=len(informacion_procesada_resultado), desc="Procesando") as pbar:
+        '''with tqdm(total=len(informacion_procesada_resultado), desc="Procesando") as pbar:
             for diccionario in informacion_procesada_resultado:
                 # Actualizar la barra de progreso
-                pbar.update(1)
+                pbar.update(1)'''
 
         # Exportar a un nuevo archivo
         exportar_a_archivo(informacion_procesada_resultado)
@@ -327,7 +392,7 @@ if __name__ == "__main__":
         print("Archivo procesado correctamente.")
         
         # Esperar a que el usuario presione Enter
-        input("\nPresiona Enter para cerrar el programa.")
+        #input("\nPresiona Enter para cerrar el programa.")
 
     except FileNotFoundError:
         print("biblio.txt no encontrado en el directorio actual. Por favor, verifica la existencia del archivo.")
@@ -336,8 +401,8 @@ if __name__ == "__main__":
         files_in_directory = os.listdir(current_directory)
         # Muestra los archivos en el directorio actual
         print("Los archivos encontrados son:", files_in_directory)
-        input("Presiona Enter para cerrar el programa.")
+        #input("Presiona Enter para cerrar el programa.")
 
     except Exception as e:
         print(f"Se produjo un error: {e}")
-        input("Presiona Enter para cerrar el programa.")
+        #input("Presiona Enter para cerrar el programa.")
